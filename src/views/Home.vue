5fd9572be544ccd543432fc4b092924926b2810d
Nikita Zhelezkov <nikita.e.zhelezkov@gmail.com>
2019-04-30 20:10:19 +0300

    fix warnings, prototype table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <!--  <div class="home">-->
+  <!--    <img alt="Vue logo" src="../assets/logo.png" />-->
+  <!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+  <!--  </div>-->
+  <Table> </Table>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Table from "../components/Table";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Table
   }
 };
 </script>
